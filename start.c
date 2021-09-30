fbeff0458c759ec9227290bdc051f31cceb561a8
kaneh1998 <22253494@student.uwa.edu.au>
2021-09-30 21:05:46 +0800

    init

---
+++ start.c
@@ -0,0 +1,16 @@
+//  CITS2002 Project 2 2021
+//  Name(s):             Kane Howard
+//  Student number(s):   22253494
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+    printf("Line 1\n");
+
+    exit(0);
+}
